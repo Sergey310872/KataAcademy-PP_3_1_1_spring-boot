414903fe1116a31af23c83ff0b06224ade3f803f
Sergey310872 <sergey310872@yandex.ru>
2024-09-16 12:43:18 +0300

    Варивнт на проверку

--- src/main/java/web/PP_3_1_1_spring_boot/DAO/UserRepository.java
+++ src/main/java/web/PP_3_1_1_spring_boot/DAO/UserRepository.java
@@ -1,8 +1,6 @@
 package web.PP_3_1_1_spring_boot.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 import web.PP_3_1_1_spring_boot.model.User;
 
--- src/main/java/web/PP_3_1_1_spring_boot/service/ServiceUserImp.java
+++ src/main/java/web/PP_3_1_1_spring_boot/service/ServiceUserImp.java
@@ -3,7 +3,6 @@ package web.PP_3_1_1_spring_boot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import web.PP_3_1_1_spring_boot.DAO.UserDAO;
 import web.PP_3_1_1_spring_boot.DAO.UserRepository;
 import web.PP_3_1_1_spring_boot.model.User;
 
@@ -11,8 +10,6 @@ import java.util.List;
 
 @Service
 public class ServiceUserImp implements ServiceUser {
-    @Autowired
-    private UserDAO userDAO;
 
     @Autowired
     private UserRepository userRepository;
@@ -20,26 +17,22 @@ public class ServiceUserImp implements ServiceUser {
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
-//        return userDAO.getAllUser();
     }
 
     @Override
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
-//        return userDAO.getUserById(id);
     }
 
     @Override
     @Transactional
     public void updateUser(User user) {
         userRepository.save(user);
-//        userDAO.updateUser(user);
     }
 
     @Override
     @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
-//        userDAO.deleteUser(id);
     }
 }
